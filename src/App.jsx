47430cf2cb4ed114f5a4192ac0d1c1cd3e627f95
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-10 17:36:29 +0100

    add useState to manage the articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Articoli from "./assets/components/Articoli";
 
 function App() {
+  const [articles, setArticles] = useState(Articoli);
   return (
     <>
       <div>
         <h1>Lista Articoli</h1>
         <ul>
-          {Articoli.map((article) => (
+          {articles.map((article) => (
             <li key={article.id}>{article.title}</li>
           ))}
         </ul>
