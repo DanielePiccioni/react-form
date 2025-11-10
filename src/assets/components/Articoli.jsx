5941773891b82ff6db64ed76c061b171d882316e
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-10 15:37:19 +0100

    create object array on file Articoli.jsx and import on app.jsx

---
+++ src/assets/components/Articoli.jsx
@@ -0,0 +1,9 @@
+const Articoli = [
+    { id: 1, title: "Meow Meow" },
+    { id: 2, title: "Prr Prr" },
+    { id: 3, title: "Grr Grr" },
+    { id: 4, title: "Puciu Puciu" },
+    { id: 5, title: "Meow Puciu" },
+];
+
+export default Articoli;
